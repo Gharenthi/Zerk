0d5ab6d0a26e3d073223edcfd5a50a0a1d26ccba
Gharenthi <wbodeau@gmail.com>
2016-11-08 11:00:35 -0800

    Update Fighter.java

--- zerk/entity/player/Fighter.java
+++ zerk/entity/player/Fighter.java
@@ -1,1 +1,21 @@
+package zerk.entity.player;
 
+import zerk.GameData;
+import zerk.Stats;
+import zerk.entity.Player;
+import zerk.lambda.Action;
+
+public class Fighter extends Player{
+	public Fighter(GameData data, String name){
+		this.data = data;
+		this.name = name;
+		stats = new Stats(5,5, 5,5, 10,10);
+		charClass = "fighter";
+		initLevelup();
+	}
+	
+	protected void initLevelup(){
+		levelupEffects.put(1, str -> {
+		});
+	}
+}
